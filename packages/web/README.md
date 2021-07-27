97bfe60292af5a667f0d5603e6a6cac3df62b398
Juiise Nexit <soglacen@gmail.com>
2021-07-27 09:39:57 +0300

    Intiail commit

---
+++ packages/web/README.md
@@ -0,0 +1,1 @@
+# pawcapsu landing
---
+++ packages/web/src/components/Icon.svelte
@@ -0,0 +1,10 @@
+<script>
+  // Importing modules
+  import FeatherIcons from "feather-icons";
+
+  // Exporting some variables;
+  export let name;
+  export let attrs;
+</script>
+
+{@html FeatherIcons.icons[name].toSvg(attrs)}
